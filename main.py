8d71c14d5f0cef8c09e376650de4957e51a68040
sserranop2 <s.serranop2@uniandes.edu.co>
2025-02-17 08:49:51 -0500

    main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import logic as lg
 
+
+
+
 if __name__ == "__main__":
-    filename = "robot_code.txt"  
+    
+    filename = input("digite el nombre del archivo:") + ".txt" 
     if lg.parse_program(filename):
         print("Sí")
     else:
